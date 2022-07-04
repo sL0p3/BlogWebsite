3233644dcf2729392178c3bd24e5fe565119043a
sL0p3 <shlokjakhotia@gmail.com>
2022-07-04 22:57:48 +0530

    port change

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ app.get("/posts/:postName",(req,res)=>{
   })
 })
 
+let port = process.env.PORT;
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port || 3000, function() {
+  console.log("Server running");
 });
